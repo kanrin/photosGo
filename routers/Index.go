8e88c91b1a9d3758a4e3850b5bc1a667d7367728
Kanrin <zhl1147259@gmail.com>
2018-10-25 14:47:51 +0800

    add gitignore

--- routers/Index.go
+++ routers/Index.go
@@ -5,6 +5,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"../libs"
 	"log"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -12,5 +13,6 @@ func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("content-type", "application/json")
+	time.Sleep(time.Duration(5)*time.Second)
 	w.Write(libs.GetPhotos(r.Host))
 }
